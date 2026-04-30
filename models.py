65803023df4474570361f555b98a51ba423ad876
TshegoM02 <tmc.mnguni@gmail.com>
2026-04-30 16:00:08 +0200

    Added docs

--- models.py
+++ models.py
@@ -1,18 +1,19 @@
 class Bug:
     # Constructor. Values a bug must receive when created
-    def __init__(self, bug_id, title, description, severity): # Object (bug, its values)
-        self.id = bug_id
-        self.title = title
+    def __init__(self, bug_id, title, description, severity): # Object (this specific bug, its values)
+        self.id = bug_id # Store bug_id in self.id, to use later in BugTracker
+        self.title = title 
         self.description = description
         self.severity = severity
-        self.status = "Open"
-        self.comment = []
+        self.status = "Open" # Every bug automatically starts as "Open"
+        self.comment = [] # Each bug has a list of comments, but starts empty
 
-    def add_comment(self, comment):
-        self.comment.append(comment)
+    # Method to add a comment to the bug
+    def add_comment(self, comment): # Function will add a comment to the bug's comment list
+        self.comment.append(comment) # Take comment and add to the bug's comment list
 
-    def update_status(self, new_status):
-        self.status = new_status
+    def update_status(self, new_status): # Function to update the bug's status
+        self.status = new_status # Replace current bug's status with new status
 
-    def __str__(self):
-        return f"[{self.id}] {self.title} - {self.severity} - {self.status}"
+    def __str__(self): # Function to return a string representation of the bug, for easy printing
+        return f"[{self.id}] {self.title} - {self.severity} - {self.status}" # Formatted string
