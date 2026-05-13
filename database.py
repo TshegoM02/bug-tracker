d8d4f166c5759c5788caaded766064838cfa09cc
TshegoM02 <tmc.mnguni@gmail.com>
2026-05-13 23:23:23 +0200

    Added database.py and created the table

---
+++ database.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = sqlite3.connect("bugs.db")
+cursor = connection.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS bugs (
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    description TEXT,
+    severity TEXT,
+    status TEXT,
+)
+""")
