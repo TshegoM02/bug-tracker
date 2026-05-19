0a92fd162ed428097913ebf271d50b9ad0834789
TshegoM02 <tmc.mnguni@gmail.com>
2026-05-19 23:54:42 +0200

    Added interactive application for option 1-6 and valided for invalid input

--- main.py
+++ main.py
@@ -79,8 +79,54 @@ if __name__ == "__main__": # Only run this code if this file is run directly
         if choice == "1":
             title = input("Enter bug title: ")
             description = input("Enter description: ")
-            severity = input("Enter severity (Low/Medium/High): ")
+            while True:
+                severity = input("Enter severity (Low/Medium/High): ").title()
+                if severity in["Low", "Medium", "High"]:
+                    break
+                print("Invalid severity. Please choose Low, Medium, or High.")
 
             bug_id = tracker.create_bug(title, description, severity)
 
             print(f"Bug created with ID {bug_id}")
+
+        elif choice == "2":
+            tracker.list_bugs()
+
+        elif choice == "3":
+            bug_id = int(input("Enter bug ID: "))
+            while True:
+                new_status = input("Enter new status (Open/In Progress/Resolved): ").title()
+                if new_status in ["Open", "In Progress", "Resolved"]:
+                    break
+                print("Invalid status. Please choose Open, In Progress, or Resolved.")
+                bug = tracker.find_bug_by_id(bug_id)
+                if bug:
+                    tracker.update_bug_status(bug_id, new_status)
+                    print("Status updated.")
+                else:
+                    print("Bug not found.")            
+
+        elif choice == "4":
+            bug_id = int(input("Enter bug ID: "))
+            comment = input("Enter comment: ")
+            bug = tracker.find_bug_by_id
+            if bug:
+                tracker.add_bug_comment(bug_id, comment)
+                print("Comment added.")
+            else:
+                print("Bug not found.")
+
+        elif choice == "5":
+            bug_id = int(input("Enter bug ID: "))
+            bug = tracker.find_bug_by_id(bug_id)
+            if bug:
+                print(f"\nComments for Bug {bug_id}:")
+                tracker.show_bug_comments(bug_id)
+            else:
+                print("Bug not found.")
+
+        elif choice == "6":
+            print("Goodbye!")
+            break
+    else:
+        print("Invalid choice. Please choose 1-6.")
