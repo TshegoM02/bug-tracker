1a37bd021a2097674af9d36e7a70f2bb0b627c51
TshegoM02 <tmc.mnguni@gmail.com>
2026-05-21 22:33:43 +0200

    Refactored for Separation of Concerns

--- main.py
+++ main.py
@@ -1,65 +1,6 @@
 from models import Bug # Bring somthing from another file, in this case the Bug class from models
 import sqlite3
-
-# This will manage multiple bugs
-class BugTracker:
-    def __init__(self): # Runs when a tracker is craeted
-        self.connection = sqlite3.connect("bugs.db") # Connect to the database file, if it doesn't exist it will be created
-        self.cursor = self.connection.cursor()
-
-    def create_bug(self, title, description, severity): # This function creates a new bug with the given details
-        self.cursor.execute("""
-        INSERT INTO bugs (title, description, severity, status)
-        VALUES (?, ?, ?, ?)
-        """, (title, description, severity, "Open"))
-        
-        self.connection.commit()
-
-        return self.cursor.lastrowid
-    
-    def find_bug_by_id(self, bug_id):
-        self.cursor.execute(
-            "SELECT * FROM bugs WHERE id = ?",
-            (bug_id,)
-        )
-        
-        return self.cursor.fetchone()
-
-    def update_bug_status(self, bug_id, new_status):
-        self.cursor.execute("""
-        UPDATE bugs
-        SET status = ?
-        WHERE id = ?
-        """, (new_status, bug_id))
-
-        self.connection.commit()
-    
-    def add_bug_comment(self, bug_id, comment):
-        self.cursor.execute("""
-        INSERT INTO comments (bug_id, comment)
-        VALUES (?, ?)
-        """, (bug_id, comment))
-
-        self.connection.commit()
-
-    def show_bug_comments(self, bug_id):
-        self.cursor.execute("""
-        SELECT comment FROM comments
-        WHERE bug_id = ?
-        """, (bug_id,))
-
-        comments = self.cursor.fetchall()
-        
-        for comment in comments:
-            print(comment[0])
-
-    def list_bugs(self):
-        self.cursor.execute("SELECT * FROM bugs")
-        
-        bugs = self.cursor.fetchall()
-        
-        for bug in bugs: # For loop to go through each bug in the list
-            print(bug) 
+from tracker import BugTracker
 
 # Test manually
 if __name__ == "__main__": # Only run this code if this file is run directly
@@ -132,4 +73,3 @@ if __name__ == "__main__": # Only run this code if this file is run directly
             break
         else:
             print("Invalid option. Please choose an option from 1-6.")
-        
--- models.py
+++ models.py
@@ -1,19 +1,11 @@
 class Bug:
-    # Constructor. Values a bug must receive when created
-    def __init__(self, bug_id, title, description, severity): # Object (this specific bug, its values)
-        self.id = bug_id # Store bug_id in self.id, to use later in BugTracker
-        self.title = title 
+
+    def __init__(self, bug_id, title, description, severity, status="Open"):
+        self.id = bug_id
+        self.title = title
         self.description = description
         self.severity = severity
-        self.status = "Open" # Every bug automatically starts as "Open"
-        self.comment = [] # Each bug has a list of comments, but starts empty
+        self.status = status
 
-    # Method to add a comment to the bug
-    def add_comment(self, comment): # Function will add a comment to the bug's comment list
-        self.comment.append(comment) # Take comment and add to the bug's comment list
-
-    def update_status(self, new_status): # Function to update the bug's status
-        self.status = new_status # Replace current bug's status with new status
-
-    def __str__(self): # Function to return a string representation of the bug, for easy printing
-        return f"[{self.id}] {self.title} - {self.severity} - {self.status}" # Formatted string
+def __str__(self): # Controls how the bug is printed.
+    return f"[{self.id}] {self.title} - {self.severity} - {self.status}"       
