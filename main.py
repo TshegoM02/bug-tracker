03e9cc8c5856e056616174b9ea16fe597fec33ff
TshegoM02 <tmc.mnguni@gmail.com>
2026-05-21 20:36:32 +0200

    Debugged menu options

--- main.py
+++ main.py
@@ -79,9 +79,10 @@ if __name__ == "__main__": # Only run this code if this file is run directly
         if choice == "1":
             title = input("Enter bug title: ")
             description = input("Enter description: ")
+
             while True:
                 severity = input("Enter severity (Low/Medium/High): ").title()
-                if severity in["Low", "Medium", "High"]:
+                if severity in ["Low", "Medium", "High"]:
                     break
                 print("Invalid severity. Please choose Low, Medium, or High.")
 
@@ -94,22 +95,23 @@ if __name__ == "__main__": # Only run this code if this file is run directly
 
         elif choice == "3":
             bug_id = int(input("Enter bug ID: "))
+            
             while True:
                 new_status = input("Enter new status (Open/In Progress/Resolved): ").title()
                 if new_status in ["Open", "In Progress", "Resolved"]:
                     break
                 print("Invalid status. Please choose Open, In Progress, or Resolved.")
-                bug = tracker.find_bug_by_id(bug_id)
-                if bug:
-                    tracker.update_bug_status(bug_id, new_status)
-                    print("Status updated.")
-                else:
-                    print("Bug not found.")            
+            bug = tracker.find_bug_by_id(bug_id)
+            if bug:
+                tracker.update_bug_status(bug_id, new_status)
+                print("Status updated.")
+            else:
+                print("Bug not found.")            
 
         elif choice == "4":
             bug_id = int(input("Enter bug ID: "))
             comment = input("Enter comment: ")
-            bug = tracker.find_bug_by_id
+            bug = tracker.find_bug_by_id(bug_id)
             if bug:
                 tracker.add_bug_comment(bug_id, comment)
                 print("Comment added.")
