314aac94c3efa9e7443031d6a40c2c223a3812fc
TshegoM02 <tmc.mnguni@gmail.com>
2026-05-23 22:40:44 +0200

    Continuing with testing

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -4,10 +4,16 @@ from tracker import BugTracker
 class TestBugTracker(unittest.TestCase):
     def test_create_bug(self):
         tracker = BugTracker()
-
         bug_id = tracker.create_bug("Login Issue", "User cannot login", "High")
-
         bug = tracker.find_bug_by_id(bug_id)
 
         self.assertIsNotNone(bug)
+
+    def test_update_bug_status(self):
+        tracker = BugTracker()
+        bug_id = tracker.create_bug("Login Issue", "User cannot login", "High")
+        tracker.update_bug_status(bug_id, "Resolved")
+        bug = tracker.find_bug_by_id(bug_id)
+
+        self.assertEqual(bug[4], "Resolved")
 
