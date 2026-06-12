0c21401a2bedf79fbbd41607821b0f5ed0154e89
TshegoM02 <tmc.mnguni@gmail.com>
2026-06-12 15:26:13 +0200

    Added setUp function

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -6,24 +6,21 @@ class TestBugTracker(unittest.TestCase):
         self.tracker = BugTracker()
         
     def test_create_bug(self):
-        tracker = BugTracker()
-        bug_id = tracker.create_bug("Login Issue", "User cannot login", "High")
-        bug = tracker.find_bug_by_id(bug_id)
+        bug_id = self.tracker.create_bug("Login Issue", "User cannot login", "High")
+        bug = self.tracker.find_bug_by_id(bug_id)
 
         self.assertIsNotNone(bug)
 
     def test_update_bug_status(self):
-        tracker = BugTracker()
-        bug_id = tracker.create_bug("Login Issue", "User cannot login", "High")
-        tracker.update_bug_status(bug_id, "Resolved")
-        bug = tracker.find_bug_by_id(bug_id)
+        bug_id = self.tracker.create_bug("Login Issue", "User cannot login", "High")
+        self.tracker.update_bug_status(bug_id, "Resolved")
+        bug = self.tracker.find_bug_by_id(bug_id)
 
         self.assertEqual(bug[4], "Resolved")
 
     def test_add_bug_comment(self):
-        tracker = BugTracker()
-        bug_id = tracker.create_bug("UI Issue", "Button btoken", "Low")
-        tracker.add_bug_comment(bug_id, "Developer investigating")
-        comments = tracker.show_bug_comments(bug_id)
-        self.assertEqual(len(comments), 1)
+        bug_id = self.tracker.create_bug("UI Issue", "Button btoken", "Low")
+        self.tracker.add_bug_comment(bug_id, "Developer investigating")
+        comments = self.tracker.show_bug_comments(bug_id)
 
+        self.assertEqual(len(comments), 1)
